dfed32321dfc3f12ac699813a892ed68623620f2
Nonso <edokashopify@gmail.com>
2025-04-02 18:27:31 +0200

    excepcionesSocio acabado

--- ProyectoBiblioteca/src/Biblioteca/excepciones/ExcepcionesSocio.java
+++ ProyectoBiblioteca/src/Biblioteca/excepciones/ExcepcionesSocio.java
@@ -1,5 +1,20 @@
 package Biblioteca.excepciones;
 
-public class ExcepcionesSocio {
+public class ExcepcionesSocio extends Exception{
+	public static final String ERROR_INSERTAR_SOCIO = "Ya existe un socio con ese código.";
+	public static final String ERROR_ELIMINAR_ = "No existe ningún socio con ese código en la base de datos.";
+	public static final String ERROR_CONSULTAR_SOCIOS_PRESTADOS = "El socio está referenciado en un préstamo de la base de datos.";
+	public static final String ERROR_CONSULTAR_SOCIO_LOCALIDAD = "No existe ningún socio con esa localidad en la base de datos.";
+	public static final String ERROR_CONSULTAR_SOCIO_SIN_PRESTAMOS = "No existe ningún socio sin préstamos en la base de datos. ";
+	public static final String ERROR_CONSULTAR_SOCIO_CON_PRESTAMOS_POR_FECHA = "No existe ningún socio con préstamos en esa fecha en la base de datos.";
+	public static final String ERROR_ABRIR_CONEXION = "Error al abrir conexion ";
+	public static final String ERROR_QUERY = "Error en la consulta ";
+	public static final String ERROR_CERRAR_CONEXION = "Error al cerrar conexion ";
+	public static final String ERROR_CARGAR_DRIVER = "Error al cargar driver";
+	
+	public ExcepcionesSocio(String mensaje) {
+		super("Error"+mensaje);
+	}
+	
 
 }
