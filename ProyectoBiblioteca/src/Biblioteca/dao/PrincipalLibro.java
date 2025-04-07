5e59729f32b581e0a8ba5a38ffa28cdc31a919c8
CesarBE06 <cesarbelenguer06@gmail.com>
2025-04-07 17:25:01 +0200

    PrincipalLibro.java

--- ProyectoBiblioteca/src/Biblioteca/dao/PrincipalLibro.java
+++ ProyectoBiblioteca/src/Biblioteca/dao/PrincipalLibro.java
@@ -19,117 +19,120 @@ public class PrincipalLibro {
 			System.out.println("1. Insertar un libro en la base de datos.");
 			System.out.println("2. Eliminar un libro, por código, de la base de datos.");
 			System.out.println("3. Consultar todos los libros de la base de datos.");
-			System.out.println("4. Consultar varios libros, por escritor, de la base de datos, ordenados por puntuación decendente.");
+			System.out.println("4. Consultar varios libros, por escritor, de la base de datos, ordenados por puntuación descendiente.");
 			System.out.println("5. Consultar los libros no prestados de la base de datos. ");
 			System.out.println("6. Consultar los libros devueltos, en una fecha, de la base de datos. ");
 			opcion = Teclado.leerEntero("Seleccione una opcion:");
-			
-			
-			switch(opcion) {
-			case 0:
-				System.out.println("Saliendo del programa...");
-				break;
-			case 1:
-				try {
-					int codigo = Teclado.leerEntero("Codigo del libro: ");
-					String isbn = Teclado.leerCadena("ISBN del libro: ");
-					String titulo = Teclado.leerCadena("Titulo del libro");
-					String escritor = Teclado.leerCadena("Escritor del libro");
-					int añoPublicacion = Teclado.leerEntero("Año de publicacion del libro: ");
-					double puntuacion = Teclado.leerReal("Puntuacion del libro: ");
-					Libro libro  = new Libro(codigo,isbn,titulo,escritor,añoPublicacion,puntuacion);
+			try {
+				switch(opcion) {
+				case 0:
+					System.out.println("Saliendo del programa...");
+					break;
+				case 1:
+					try {
+						String isbn = Teclado.leerCadena("ISBN del libro: ");
+						String titulo = Teclado.leerCadena("Titulo del libro");
+						String escritor = Teclado.leerCadena("Escritor del libro");
+						int añoPublicacion = Teclado.leerEntero("Año de publicacion del libro: ");
+						double puntuacion = Teclado.leerReal("Puntuacion del libro: ");
+						Libro libro  = new Libro(isbn,titulo,escritor,añoPublicacion,puntuacion);
 
-					GestorLibros.insertarLibro(libro);
+						GestorLibros.insertarLibro(libro);
 
-					System.out.println("Se ha insertado un libro en la base de datos.");
+						System.out.println("Se ha insertado un libro en la base de datos.");
 
-				}catch (BDException e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-			case 2:
-				try {
-
-					int codigo = Teclado.leerEntero("Codigo del libro: ");
-
-					GestorLibros.eliminarLibro(codigo);
-
-					System.out.println("Se ha eliminado un libro de la base de datos.");
-
-				}catch (BDException e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-			case 3:
-
-				try {
-					libros = GestorLibros.consultarLibros();
-
-					if (libros.size() == 0) {
-						throw new ExcepcionesLibro(ExcepcionesLibro.ERROR_CONSULTAR_LIBROS);
-					} else {
-						for (Libro libro : libros) {
-							System.out.println(libro.toString());
-						}
-						System.out.println("Se han consultado " + libros.size() + " departamentos de la base de datos.");
+					}catch (BDException e) {
+						System.out.println(e.getMessage());
 					}
-				} catch (BDException e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-			case 4:
-				try {
-					libros = GestorLibros.consultarLibroPorEscritorOrdenadosPorPuntuacion();
+					break;
+				case 2:
+					try {
 
-					if(libros.size() == 0) {
-						throw new ExcepcionesLibro(ExcepcionesLibro.ERROR_CONSULTAR_LIBRO_ESCRITOR);
-					}else {
-						for (Libro libro : libros) {
-							System.out.println(libro.toString());
-						}
-						System.out.println("Se han consultado " + libros.size() + " departamentos de la base de datos.");
+						int codigo = Teclado.leerEntero("Codigo del libro: ");
+
+						GestorLibros.eliminarLibro(codigo);
+
+						System.out.println("Se ha eliminado un libro de la base de datos.");
+
+					}catch (BDException e) {
+						System.out.println(e.getMessage());
+					}catch(ExcepcionesLibro l) {
+						System.out.println(l.getMessage());
 					}
-				}catch (BDException e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-			case 5:
-				try {
-					libros = GestorLibros.librosNoPrestados();
+					break;
+				case 3:
 
-					if(libros.size() == 0) {
-						throw new ExcepcionesLibro(ExcepcionesLibro.ERROR_LIBROS_NOPRESTADOS);
-					}else {
-						for (Libro libro : libros) {
-							System.out.println(libro.toString());
+					try {
+						libros = GestorLibros.consultarLibros();
+
+						if (libros.size() == 0) {
+							throw new ExcepcionesLibro(ExcepcionesLibro.ERROR_CONSULTAR_LIBROS);
+						} else {
+							for (Libro libro : libros) {
+								System.out.println(libro.toString());
+							}
+							System.out.println("Se han consultado " + libros.size() + " libros de la base de datos.");
 						}
-						System.out.println("Se han consultado " + libros.size() + " departamentos de la base de datos.");
+					} catch (BDException e) {
+						System.out.println(e.getMessage());
 					}
-				}catch (BDException e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-			case 6:
-				try {
-					
-					String fechaDevolucion = Teclado.leerCadena("Introduce la fecha de devolucion(YYYY-MM-DD): ");
-					libros = GestorLibros.librosDevueltosFecha(fechaDevolucion);
+					break;
+				case 4:
+					try {
+						libros = GestorLibros.consultarLibroPorEscritorOrdenadosPorPuntuacion();
 
-					if(libros.size() == 0) {
-						throw new ExcepcionesLibro(ExcepcionesLibro.ERROR_LIBROS_NODEVUELTOS);
-					}else {
-						for (Libro libro : libros) {
-							System.out.println(libro.toString());
+						if(libros.size() == 0) {
+							throw new ExcepcionesLibro(ExcepcionesLibro.ERROR_CONSULTAR_LIBRO_ESCRITOR);
+						}else {
+							for (Libro libro : libros) {
+								System.out.println(libro.toString());
+							}
+							System.out.println("Se han consultado " + libros.size() + " libros de la base de datos.");
 						}
-						System.out.println("Se han consultado " + libros.size() + " departamentos de la base de datos.");
+					}catch (BDException e) {
+						System.out.println(e.getMessage());
 					}
-				}catch (BDException e) {
-					System.out.println(e.getMessage());
-				}
-				break;
+					break;
+				case 5:
+					try {
+						libros = GestorLibros.librosNoPrestados();
 
-			default:
-				System.out.println("Opcion no valida.");	
+						if(libros.size() == 0) {
+							throw new ExcepcionesLibro(ExcepcionesLibro.ERROR_LIBROS_NOPRESTADOS);
+						}else {
+							for (Libro libro : libros) {
+								System.out.println(libro.toString());
+							}
+							System.out.println("Se han consultado " + libros.size() + " libros de la base de datos.");
+						}
+					}catch (ExcepcionesLibro l) {
+						System.out.println(l.getMessage());
+					}
+					break;
+				case 6:
+					try {
+
+						String fechaDevolucion = Teclado.leerCadena("Introduce la fecha de devolucion(YYYY-MM-DD): ");
+						libros = GestorLibros.librosDevueltosFecha(fechaDevolucion);
+
+						if(libros.size() == 0) {
+							throw new ExcepcionesLibro(ExcepcionesLibro.ERROR_LIBROS_NODEVUELTOS);
+						}else {
+							for (Libro libro : libros) {
+								System.out.println(libro.toString());
+							}
+							System.out.println("Se han consultado " + libros.size() + " libros de la base de datos.");
+						}
+					}catch (ExcepcionesLibro l) {
+						System.out.println(l.getMessage());
+					}
+					break;
+
+				default:
+					System.out.println("Opcion no valida.");	
+				}
+			}catch(ExcepcionesLibro l) {
+				System.out.println(l.getMessage());
 			}
 		}while(opcion !=0);
 	}
